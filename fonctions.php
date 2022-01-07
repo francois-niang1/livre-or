f5cde2e6ff4a64905dce0c44506dd1e4e3108397
francois-niang1 <francois.niang@laplateforme.io>
2022-01-07 12:23:34 +0100

    index, inscrption connexion profil fait

--- fonctions.php
+++ fonctions.php
@@ -76,4 +76,75 @@ function deconnect(){
         exit();
     }
 }
+function ChangeLogin(){
+    if (isset($_SESSION['user'])) {
+        $username = $_SESSION['user']['login'];
+        if (isset($_POST['changelog'])) {
+            $login = $_POST['login'];
+            $user_login = $_SESSION['user']['login'];
+            $user_id = $_SESSION['user']['id'];
+            $newlogin = $_POST['newlogin'];
+            $repeatnewlogin = $_POST['repeatnewlogin'];
+            if ($login && $newlogin && $repeatnewlogin) {
+                if ($newlogin == $repeatnewlogin) {
+                    $Bdd = mysqli_connect('localhost', 'root', 'root', 'livreor') or die('Erreur');
+                    $Requete = mysqli_query($Bdd, "SELECT * FROM utilisateurs WHERE login = '$username' AND login = '$login'");
+                    $rows = mysqli_num_rows($Requete);
+                    if ($newlogin == $user_login){
+                        echo "<p>Votre ancien Login est identique</p><style>p{color : var(--White-); font-size: 1em;}</style>";
+                    }
+                    else if ($rows==1) {
+                        $newpre = mysqli_query($Bdd, "UPDATE utilisateurs SET login='$newlogin' WHERE login='$username'");
+                        session_destroy();
+                        header('Location: connexion.php');
+                        exit();
+                    }
+                    else{
+                        echo "<p>Votre ancien Login est incorrect</p><style>p{color : var(--White-); font-size: 1em;}</style>";
+                    }
+                }
+                else{
+                    echo "<p>Les deux champs doivent être identiques</p><style>p{color : var(--White-); font-size: 1em;}</style>";
+                }
+            }
+            else{
+                echo "<p>Veuillez saisir tous les champs</p><style>p{color : var(--White-); font-size: 1em;}</style>";
+            }
+        }
+    }
+    else{
+        header("Location:connexion.php");
+        exit();
+    }
+}
+function ChangeMdp(){
+    $Bdd = connect_database();
+    if (isset($_POST['password']) && isset($_POST['newpassword']) && isset($_POST['repeatnewpassword'])){
+        $id = $_SESSION['user']['id'];
+        $password = $_SESSION['user']['password'];
+        $oldpassword = $_POST['password'];
+        $newpassword = $_POST['newpassword'];
+        $repeatnewpassword = $_POST['repeatnewpassword'];
+        $new_password_hash = password_hash($newpassword, PASSWORD_DEFAULT);
+        if ($password == NULL || $newpassword == NULL || $repeatnewpassword == NULL ){
+            echo "<p>Veuillez saisir tous les champs</p><style>p{color : var(--White-); font-size: 1em;}</style>";
+        }
+        else if(password_verify($oldpassword, $password) == false){
+            echo "<p>Votre ancien Password est incorrect</p><style>p{color : var(--White-); font-size: 1em;}</style>" ;
+        }
+        else if ($newpassword != $repeatnewpassword){
+            echo "<p>Les deux champs doivent être identiques</p><style>p{color : var(--White-); font-size: 1em;}</style>";
+        }
+        else{
+            $requete_select_pwd = mysqli_query($Bdd, "SELECT * FROM `utilisateurs` WHERE password = '$password' ");
+            $requete_update_pwd = mysqli_query($Bdd, "UPDATE utilisateurs SET password = '$new_password_hash' WHERE id = '$id'");
+            session_destroy();
+            header('Location: connexion.php');
+            exit();
+        }
+    }
+    else{
+        echo ' ';
+    }
+}
 ?>
--- profil.php
+++ profil.php
@@ -2,7 +2,7 @@
 session_start();
 require 'fonctions.php';
 if(!isset($_SESSION['user'])){
-    header('Location: index.php');
+    header('Location: connexion.php');
 }
 deconnect();
 ?>
@@ -13,18 +13,37 @@ deconnect();
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/profil.css">
+        <link rel="stylesheet" href="css/root&font.css">
+        <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+        <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
         <title>Mon Profil</title>
     </head>
     <body>
         <?php require ('header.php');?>
         <main>
             <div class="Container">
-                <div class="box-change-info">
-                    <div class="box-change">
-                        <h2 class="welcome-msg"><?php if(isset($_SESSION['user']['login'])){echo 'Bonjour, <br><a>'.$_SESSION['user']['login'].'</a> vous etes bien connecte.';}?></h2>
-                        <a href = "change-login.php"><button class="modif">Modifier votre Login</button></a>
-                        <a href = "change-email.php"><button class="modif">Modifier votre Email</button></a>
-                        <a href = "change-password.php"><button class="modif">Modifier votre Mot de passe</button></a>
+                <div class="parallax">
+                    <h1 data-aos="flip-left" data-aos-duration="3000">Bonjour, ravi de vous voir <?php echo $_SESSION['user']['login'];?><br>
+                    ❗❗ ATTENTION ❗❗<br>
+                    Si vous changez vos informatons vous devrez vous reconnecter !!</h1>
+                    <div class="box" data-aos="fade-up" data-aos-duration="3000">
+                        <div class="form-change">
+                            <form action="" method = "POST" >
+                                <input class="input" type="text" name="login" placeholder = 'Votre ancien Login'>
+                                <input class="input" type="text" name="newlogin" placeholder = 'Votre nouveau Login'>
+                                <input class="input" type="text" name="repeatnewlogin" placeholder = 'Répétez votre nouveau Login'><br/><br/>
+                                <?php ChangeLogin();?>
+                                <button class="modif" type="submit" value="Valider" name = 'changelog' >Changer le Login</button>
+                            </form>
+                            <form action="" method = "POST" >
+                                <input class="input" type="password" name="password" placeholder = 'Votre ancien Password'>
+                                <input class="input" type="password" name="newpassword" placeholder = 'Votre nouveau Password'>
+                                <input class="input" type="password" name="repeatnewpassword" placeholder = 'Répétez votre nouveau Password'><br/><br/>
+                                <?php ChangeMdp();?>
+                                <button class="modif" type="submit" value="Valider" name = 'submit' >Changer le Mot de Passe </button>
+                            </form>
+                        </div>
                         <form action="" method = "POST" ><button class = "deco" type = "submit" name = "deconnexion" value ='Deconnexion'>Déconnexion</buttton></form>
                     </div>
                 </div>
@@ -32,4 +51,7 @@ deconnect();
         </main>
         <?php require ('footer.php'); ?>
     </body>
+    <script>
+        AOS.init();
+    </script>
 </html>
